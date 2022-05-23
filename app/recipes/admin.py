8e6e035aa07d673da449973278d84aae35451474
Oleg Repalo <altwulf7@gmail.com>
2022-05-23 22:17:22 +0300

    Work with admin.py add some fields

--- app/recipes/admin.py
+++ app/recipes/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import RecipeModel, IngredientModel
 
-admin.site.register(RecipeModel)
-admin.site.register(IngredientModel)
+
+class IngredientModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'measure_unit', 'amount')
+    search_fields = ['title']
+
+
+class RecipeModelAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                 {'fields': ['title']}),
+        ('Recipe description', {'fields': ['desc'], 'classes': ['collapse']}),
+        ('Ingredients',        {'fields': ['ingredients']}),
+    ]
+    list_display = ('title', 'desc')
+    search_fields = ['title', 'desc']
+
+
+admin.site.register(RecipeModel, RecipeModelAdmin)
+admin.site.register(IngredientModel, IngredientModelAdmin)
